a289b83ea89777b3d5f55d293380cec8596dea0a
Dafna Levy <dafna@macitwork.nl>
2023-04-01 13:26:16 +0200

    git

--- functions-and-scope.js
+++ functions-and-scope.js
@@ -16,6 +16,20 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 
 // ---- Verwachte uitkomst: 6
 
+// Om het aantal studenten dat cum laude is afgestudeerd te checken, kunnen we de volgende stappen volgen:
+// 1. Initialiseer een teller op 0 om het aantal cum laude afgestudeerden bij te houden.
+// 2. Loop door de array met cijfers.
+// 3. Voor elk cijfer dat 8 of hoger is, verhoog de teller met 1.
+// 4. Log het aantal cum laude afgestudeerden in de terminal.
+
+// Implementatie:
+let cumLaudeCount = 0;
+for (let i = 0; i < grades.length; i++) {
+    if (grades[i] >= 8) {
+        cumLaudeCount++;
+    }
+}
+console.log(cumLaudeCount); // Output: 6
 
 /*  1b: Omschrijven tot een herbruikbare functie   */
 // Schrijf een functie genaamd cumLaude, die een array van cijfers verwacht (zoals grades) en het aantal Cum laude studenten teruggeeft. Gebruik hiervoor jouw antwoord van 1a.
@@ -27,7 +41,19 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // cumLaude([6, 4, 5]) geeft 0
 // cumLaude([8, 9, 4, 6, 10]) geeft 3
 
+function cumLaude(grades) {
+    let count = 0;
+    for (let i = 0; i < grades.length; i++) {
+        if (grades[i] >= 8) {
+            count++;
+        }
+    }
+    return count;
+}
 
+console.log(cumLaude([9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6])); // verwachte uitkomst: 6
+console.log(cumLaude([6, 4, 5])); // verwachte uitkomst: 0
+console.log(cumLaude([8, 9, 4, 6, 10])); // verwachte uitkomst: 3
 
 
 /* Opdracht  2: Gemiddeld cijfer */
@@ -39,8 +65,23 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // * Wat moet ik verzamelen uit de array van cijfers om uiteindelijk een gemiddelde te kunnen berekenen?
 // * Hoe zorgt ik ervoor dat ik alle waardes uit de array kan langslopen, ook als de array wel 100 entries zou bevatten?
 // Log het antwoord in de terminal.
+//  +++++++++++++++++++++++++++++++++
+ //   Om het gemiddelde eindcijfer te berekenen uit de grades array moeten de volgende stappen worden uitgevoerd:
 
+ //   Definieer een variabele sum en zet deze op 0.
+ //  Loop door de array heen en tel bij iedere iteratie het cijfer op bij sum.
+ //   Bepaal het gemiddelde door de som te delen door het aantal cijfers in de array (grades.length).
+ //   Log het gemiddelde in de terminal.
+ //   ++++++++++++++++++++++++++++++
 // ---- Verwachte uitkomst: 6.642857142857143
+let sum = 0;
+
+for (let i = 0; i < grades.length; i++) {
+    sum += grades[i];
+}
+
+const average = sum / grades.length;
+console.log(average); // 6.642857142857143
 
 
 /* 2b: Omschrijven tot een herbruikbare functie */
@@ -53,12 +94,59 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // averageGrade([6, 4, 5]) geeft xxxx
 // averageGrade([8, 9, 4, 6, 10]) geeft xxxx
 
+//Om de gemiddelde waarde van een array van cijfers te berekenen, kunnen we de volgende stappen uitvoeren:
+//
+//     Maak een variabele genaamd total en zet deze op 0.
+//     Loop over de array en tel elke waarde op bij de total variabele.
+//     Deel de total variabele door het aantal elementen in de array om het gemiddelde te berekenen.
+//     Return het gemiddelde.
+//
+// Hier is de implementatie van de averageGrade functie:
+function averageGrade(grades) {
+    let total = 0;
+    for (let i = 0; i < grades.length; i++) {
+        total += grades[i];
+    }
+    const average = total / grades.length;
+    return average;
+}
+
+//We kunnen deze functie gebruiken om het gemiddelde cijfer van de grades array te berekenen:
+
+    //arduino
+
+console.log(averageGrade(grades)); // geeft 6.642857142857143
+
+//En we kunnen ook andere arrays doorgeven aan de functie:
+
+   // arduino
+
+console.log(averageGrade([6, 4, 5])); // geeft 5
+console.log(averageGrade([8, 9, 4, 6, 10])); // geeft 7.4
+
+
 
 /* 2c: Afronden op twee decimalen */
 // Zorg ervoor dat het gemiddelde cijfer dat wordt teruggegeven uit de functie netjes wordt afgerond op twee decimalen.
 // Tip: Google is your best friend!
 
+//Om een getal af te ronden op twee decimalen kun je de toFixed() methode gebruiken. Deze methode neemt één argument, namelijk het aantal decimalen waarop het getal afgerond moet worden.
 
+   // Hieronder zie je hoe je toFixed() kunt gebruiken om het gemiddelde cijfer netjes af te ronden op twee decimalen:
+
+
+function averageGrade(grades) {
+    let total = 0;
+    for (let i = 0; i < grades.length; i++) {
+        total += grades[i];
+    }
+    let average = total / grades.length;
+    return average.toFixed(2);
+}
+
+console.log(averageGrade(grades)); // geeft 6.64
+
+//In dit voorbeeld roepen we de toFixed() methode aan op de average variabele. Hierdoor wordt het gemiddelde cijfer afgerond op twee decimalen. Let op: de toFixed() methode retourneert een string, geen getal.
 
 
 /* Bonusopdracht: hoogste cijfer */
@@ -71,7 +159,26 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // Log het antwoord in de terminal.
 
 // ---- Verwachte uitkomst: 9
+//Om het hoogst behaalde cijfer te vinden op basis van de grades array, kunnen we de volgende stappen nemen:
 
+   //// We maken een variabele highestGrade aan en zetten deze op 0. Dit is de waarde waar we het hoogst behaalde cijfer in gaan opslaan.
+   // We gaan de grades array langs met behulp van een for-loop. Voor ieder cijfer in de array doen we het volgende:
+  //  a. We checken of het huidige cijfer hoger is dan highestGrade.
+ //   b. Als dat zo is, slaan we het huidige cijfer op in highestGrade.
+ //   We loggen highestGrade in de console.
+
+   // Hieronder staat het script dat deze stappen uitvoert:
+const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
+
+let highestGrade = 0;
+
+for (let i = 0; i < grades.length; i++) {
+    if (grades[i] > highestGrade) {
+        highestGrade = grades[i];
+    }
+}
+
+console.log(highestGrade);
 
 /* 3b: Omschrijven tot een herbruikbare functie */
 // Schrijf een functie genaamd highestGrade, die een array van cijfers verwacht (zoals grades) en het hoogste cijfer teruggeeft. Gebruik hiervoor jouw antwoord van 3a.
@@ -82,3 +189,21 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // highestGrade(grades) geeft 9
 // highestGrade([6, 4, 5]) geeft 6
 // highestGrade([8, 9, 4, 6, 10]) geeft 10
+
+
+function highestGrade(grades) {
+    let highest = grades[0]; // Set the initial value to the first element in the array
+    for (let i = 1; i < grades.length; i++) { // Start the loop at index 1 (the second element in the array)
+        if (grades[i] > highest) { // Check if the current element is higher than the current highest value
+            highest = grades[i]; // If so, set the current element as the new highest value
+        }
+    }
+    return highest; // Return the highest value found in the array
+}
+const grades1 = [8, 6, 7, 9, 5, 8, 9];
+const grades2 = [6, 4, 5];
+const grades3 = [8, 9, 4, 6, 10];
+
+console.log(highestGrade(grades1)); // Output: 9
+console.log(highestGrade(grades2)); // Output: 6
+console.log(highestGrade(grades3)); // Output: 10
--- functions-methods.js
+++ functions-methods.js
@@ -10,6 +10,19 @@
 // getEmailDomain("a.wiersma@outlook.com") geeft outlook.com
 
 
+function getEmailDomain(email) {
+    const atIndex = email.indexOf('@');
+    if (atIndex !== -1) {
+        return email.slice(atIndex + 1);
+    } else {
+        return null; // Geen geldig emailadres
+    }
+}
+
+// Testen van de functie
+console.log(getEmailDomain("n.eeken@novi-education.nl")); // verwachte uitkomst: novi-education.nl
+console.log(getEmailDomain("t.mellink@novi.nl")); // verwachte uitkomst: novi.nl
+console.log(getEmailDomain("a.wiersma@outlook.com")); // verwachte uitkomst: outlook.com
 
 
 /* Opdracht  2 */
@@ -19,7 +32,24 @@
 // typeOfEmail("t.mellink@novi.nl") geeft geeft "Medewerker"
 // typeOfEmail("novi.nlaapjesk@outlook.com") geeft geeft "Extern" <-- deze moet het ook doen!
 // typeOfEmail("a.wiersma@outlook.com") geeft "Extern"
+function typeOfEmail(email) {
+    const domain = getEmailDomain(email);
+    if (!domain) {
+        return null; // Geen geldig emailadres
+    } else if (domain === 'novi.nl') {
+        return 'Medewerker';
+    } else if (domain === 'novi-education.nl') {
+        return 'Student';
+    } else {
+        return 'Extern';
+    }
+}
 
+// Testen van de functie
+console.log(typeOfEmail("n.eeken@novi-education.nl")); // verwachte uitkomst: "Student"
+console.log(typeOfEmail("t.mellink@novi.nl")); // verwachte uitkomst: "Medewerker"
+console.log(typeOfEmail("novi.nlaapjesk@outlook.com")); // verwachte uitkomst: "Extern"
+console.log(typeOfEmail("a.wiersma@outlook.com")); // verwachte uitkomst: "Extern"
 
 
 /* Opdracht  3 */
@@ -34,3 +64,22 @@
 // checkEmailValidity("n.eekenanovi.nl") geeft false - want geen @
 // checkEmailValidity("n.eeken@novinl.") geeft false - want de punt mag niet als laatst
 // checkEmailValidity("tessmellink@novi,nl") geeft false - want er staat een komma in
+
+function checkEmailValidity(email) {
+    if (!email.includes('@')) {
+        return false; // Er moet een @ in het emailadres zitten
+    } else if (email.endsWith('.')) {
+        return false; // Punt mag niet als laatste karakter in domeinnaam zitten
+    } else if (email.includes(',')) {
+        return false; // Er mag geen komma in het emailadres zitten
+    } else {
+        return true;
+    }
+}
+
+// Testen van de functie
+console.log(checkEmailValidity("n.eeken@novi.nl")); // verwachte uitkomst: true
+console.log(checkEmailValidity("tessmellink@novi.nl")); // verwachte uitkomst: true
+console.log(checkEmailValidity("n.eekenanovi.nl")); // verwachte uitkomst: false
+console.log(checkEmailValidity("n.eeken@novinl.")); // verwachte uitkomst: false
+console.log(checkEmailValidity("tessmellink@novi,nl")); // verwachte uitkomst: false
